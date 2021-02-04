aef152401c08179713932c606fc58127d5253914
C1neks <baranowskimarcin7@gmail.com>
2021-02-04 14:51:26 +0100

    ENV FILE

--- myApp.js
+++ myApp.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 
 
 
 
 
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/views/index.html");
+});
 
 
+app.use(express.static(__dirname+"/public"));
 
+
+// app.get("/json",function(req,res){
+//     res.json({"message": "Hello json"})
+// });
+
+
+app.get("/json",function(req,res){
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({"message":"HELLO JSON"}
+        )
+    }
+    else {
+        res.json({"message": "Hello json"})
+    }
+});
 
 
 
